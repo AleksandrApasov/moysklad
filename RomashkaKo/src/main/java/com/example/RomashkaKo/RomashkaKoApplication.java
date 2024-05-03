04a3a816bdf5f918d7e269ae7fb5ed06551fc198
AleksandrApasov <140314249+AleksandrApasov@users.noreply.github.com>
2024-05-03 15:28:26 +0300

    Add files via upload

---
+++ RomashkaKo/src/main/java/com/example/RomashkaKo/RomashkaKoApplication.java
@@ -0,0 +1,13 @@
+package com.example.RomashkaKo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomashkaKoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomashkaKoApplication.class, args);
+	}
+
+}
---
+++ RomashkaKo/src/test/java/com/example/RomashkaKo/RomashkaKoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RomashkaKo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomashkaKoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
